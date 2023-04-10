3355cb268c822dce950b3d45a46d78ac9db33e32
treeb23 <105105490+treeb23@users.noreply.github.com>
2023-04-11 01:17:44 +0900

    Update addGPXtoHTML.py

--- addGPXtoHTML/addGPXtoHTML.py
+++ addGPXtoHTML/addGPXtoHTML.py
@@ -18,16 +18,21 @@ def leafmap_view(latitude=35,longitude=140,zoom=4):#mapを表示
     m = leafmap.Map(center=(latitude, longitude), zoom=zoom)
     return m
 
-def addgpxtohtml(gpx_filename,outhtml_filename):
-    df = gpxdf.read_gpx(f'{f_path}/data/{gpx_filename}.gpx')
-    try:
-        df2 = pd.read_csv(f"{f_path}/data/gpxdf.csv")
-        u = df2['trackname'].unique()
+def addgpxtohtml(gpx_filename="abc",outhtml_filename="abc",mode='w'):
+    if mode=='w':
+        df = gpxdf.read_gpx(f'{f_path}/data/{gpx_filename}.gpx')
+        try:
+            df2 = pd.read_csv(f"{f_path}/data/gpxdf.csv")
+            u = df2['trackname'].unique()
+            print(f'すでに追加されたトラック一覧:{u[0:-1]}')
+            df2=df2.append(df, ignore_index=True)
+            gpxdf.to_html_map(df2, f'{f_path}/data/{outhtml_filename}.html',zoom_start=11)
+            df2.to_csv(f"{f_path}/data/gpxdf.csv",index=False)
+            print(f"data/{outhtml_filename}.htmlを作成。data/gpxdf.csvを更新")
+        except:
+            df.to_csv(f"{f_path}/data/gpxdf.csv",index=False)
+            print(f"data/gpxdf.csvを作成")
+    else:
+        df = pd.read_csv(f"{f_path}/data/gpxdf.csv")
+        u = df['trackname'].unique()
         print(f'すでに追加されたトラック一覧:{u[0:-1]}')
-        df2=df2.append(df, ignore_index=True)
-        gpxdf.to_html_map(df2, f'{f_path}/data/{outhtml_filename}.html',zoom_start=11)
-        df2.to_csv(f"{f_path}/data/gpxdf.csv",index=False)
-        print(f"data/{outhtml_filename}.htmlを作成。data/gpxdf.csvを更新")
-    except:
-        df.to_csv(f"{f_path}/data/gpxdf.csv",index=False)
-        print(f"data/gpxdf.csvを作成")
